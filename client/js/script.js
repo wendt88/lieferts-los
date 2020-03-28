1ae8fd6918702cdb6e24b900dc87dd94bd400186
wendt88 <wendt88@gmail.com>
2020-03-28 11:42:43 +0100

    set db as vue prototype

--- client/js/script.js
+++ client/js/script.js
@@ -9,10 +9,19 @@ import '@fortawesome/fontawesome-free/css/all.css'
 
 import router from './vueRouter'
 import store from './store'
+import db from './db'
 
 global.$ = global.jQuery = $
 
 Vue.use(AsyncComputed)
+
+Vue.use({
+    install: (Vue) => {
+        Object.assign(Vue.prototype, {
+            $db: db
+        })
+    }
+})
 
 new Vue({
     render: createElement => createElement('router-view'),
