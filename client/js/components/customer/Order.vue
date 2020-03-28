daef0b8ce54f8fa0cbff447cf28445a4f0bbc1bd
Flori <patflo_no1thereal@yahoo.de>
2020-03-28 12:27:53 +0100

    working on order list

--- client/js/components/customer/Order.vue
+++ client/js/components/customer/Order.vue
@@ -1,29 +1,87 @@
 <template>
-    <h1>order</h1>
+    <div>
+        <h1>Bestellung</h1>
+        <form>
+            <div class="form-group">
+                <label>Lieferant</label>
+                <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Liefernant inenspeibn"
+                >
+            </div>
+            <div
+                v-for="product of order.products"
+                :key="product.id"
+                class="row"
+            >
+                <div class="col">
+                    <div class="form-group">
+                        <label>Moß</label>
+                        <select
+                            id="exampleFormControlSelect1"
+                            class="form-control"
+                        >
+                            <option>Kilo</option>
+                            <option>Gramm</option>
+                            <option>Stuck</option>
+                            <option>Liter</option>
+                            <option>Maul voll</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <label>Menge</label>
+                        <input
+                            type="number"
+                            class="form-control"
+                            placeholder="Menge - sollet a Zohl sein bittschian 😘"
+                        >
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <label>Zuig</label>
+                        <input
+                            type="text"
+                            class="form-control"
+                            placeholder="Is Produkt woses gern hett"
+                        >
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
 </template>
 
 <script>
 export default {
     data: function () {
         return {
-            order: []
+            order: {
+                products: [
+                    {},
+                ],
+            }
         }
     },
     watch: {
         async $route (to, from) {
             if (from === 'new') {
                 this.editable = true
-                this.order = []
+                this.order = {
+                    products: [
+                        {},
+                    ],
+                }
             }
             else {
                 this.editable = false
-                // this.$firebase.
+                this.order = await this.$db.order(to)
             }
         }
     },
-    created () {
-        this.db = this.$firebase.firestore()
-    }
 }
 </script>
 
