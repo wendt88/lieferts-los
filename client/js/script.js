21e773d62bbe2509f1653b787cd0172a62e7b755
wendt88 <wendt88@gmail.com>
2020-03-28 10:00:55 +0100

    vuex & firebase added

--- client/js/script.js
+++ client/js/script.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import * as firebase from 'firebase';
 import $ from 'jquery';
 require('bootstrap/js/src/collapse');
 require('bootstrap/js/src/modal');
@@ -6,11 +7,34 @@ require('bootstrap/js/src/scrollspy');
 require('bootstrap/js/src/dropdown');
 
 import router from './vueRouter';
+import store from './store';
 
 global.$ = global.jQuery = $;
 
+const firebaseConfig = {
+    apiKey: 'AIzaSyDNOkG57a_ZYl8qbwn5YMSIO9BGjbwHkFU',
+    authDomain: 'bringr-io-dev.firebaseapp.com',
+    databaseURL: 'https://bringr-io-dev.firebaseio.com',
+    projectId: 'bringr-io-dev',
+    storageBucket: 'bringr-io-dev.appspot.com',
+    messagingSenderId: '103924518486',
+    appId: '1:103924518486:web:40cc6fd2c7c822f903b5f7',
+    measurementId: 'G-T64131CJ15'
+};
+
+firebase.initializeApp(firebaseConfig);
+
+Vue.use({
+    install: (Vue) => {
+        Object.assign(Vue.prototype, {
+            $firebase: firebase
+        });
+    }
+});
+
 new Vue({
     render: createElement => createElement('router-view'),
+    store,
     router,
 })
     .$mount('body > div');
