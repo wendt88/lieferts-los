8ded4cc5d0c7f80d17f87a12fed8658ab2fd4311
Flori <patflo_no1thereal@yahoo.de>
2020-03-28 13:07:29 +0100

    order: auto adding new products to list order: added model

--- client/js/components/customer/Order.vue
+++ client/js/components/customer/Order.vue
@@ -11,29 +11,61 @@
                 >
             </div>
             <div
-                v-for="product of order.products"
+                class="row"
+            >
+                <div class="col">
+                    <div class="form-group">
+                        <h4>Moß</h4>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <h4>Menge</h4>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <h4>Zuig</h4>
+                    </div>
+                </div>
+            </div>
+            <div
+                v-for="(product, index) of order.products"
                 :key="product.id"
                 class="row"
             >
                 <div class="col">
                     <div class="form-group">
-                        <label>Moß</label>
                         <select
                             id="exampleFormControlSelect1"
+                            v-model="order.products[index].unit"
                             class="form-control"
                         >
-                            <option>Kilo</option>
-                            <option>Gramm</option>
-                            <option>Stuck</option>
-                            <option>Liter</option>
-                            <option>Maul voll</option>
+                            <option value="kg">
+                                Kilo
+                            </option>
+                            <option value="g">
+                                Gramm
+                            </option>
+                            <option value="pieces">
+                                Stuck
+                            </option>
+                            <option value="l">
+                                Liter
+                            </option>
+                            <option value="mv">
+                                Maul voll
+                            </option>
+                            <option value="tiggisch">
+                                Wollten awian
+                            </option>
                         </select>
                     </div>
                 </div>
                 <div class="col">
                     <div class="form-group">
-                        <label>Menge</label>
                         <input
+                            v-model="order.products[index].amount"
                             type="number"
                             class="form-control"
                             placeholder="Menge - sollet a Zohl sein bittschian 😘"
@@ -42,11 +74,12 @@
                 </div>
                 <div class="col">
                     <div class="form-group">
-                        <label>Zuig</label>
                         <input
+                            v-model="order.products[index].description"
                             type="text"
                             class="form-control"
                             placeholder="Is Produkt woses gern hett"
+                            @keyup="addNewRowIfLast(index)"
                         >
                     </div>
                 </div>
@@ -56,6 +89,10 @@
 </template>
 
 <script>
+
+// TODO: photo upload per product
+// TODO: barcode scanner per product
+
 export default {
     data: function () {
         return {
@@ -82,6 +119,14 @@ export default {
             }
         }
     },
+    methods: {
+        addNewRowIfLast (index) {
+            if (this.order.products.length-1 === index) {
+                this.order.products.push({})
+            }
+            console.log(this.order.products)
+        }
+    }
 }
 </script>
 
