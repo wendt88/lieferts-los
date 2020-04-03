b8713e6c4928204bdd85be29bed45818140f12c9
danilo <dfink91@gmail.com>
2020-04-03 22:53:29 +0200

    Beim Anlegen der Orders wird jetzt eine Mail an den Shopowner gesandt. Zur zeit einfach an bringr.io@gmail.com damit koane umsusch aussi geat

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,8 @@
 const functions = require('firebase-functions')
-// const nodemailer = require('nodemailer')
+const nodemailer = require('nodemailer')
 const Firestore = require('@google-cloud/firestore')
 const Crypto = require('crypto')
+const Config = require('./config/config')
 const PROJECTID = 'bringr-io-dev'
 const ORDERS = 'orders'
 
@@ -9,35 +10,6 @@ const firestore = new Firestore({
     projectId: PROJECTID,
     timestampsInSnapshots: true,
 })
-
-// const transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//         user: Creds.MAIL_ADDRESS,
-//         pass: Creds.MAIL_APP_PASS
-//     }
-// })
-
-// const mailOptions = {
-//     from: 'bringr@gmail.com',
-//     to: 'danilo@kibun.io',
-//     subject: 'Sending Email using Node.js',
-//     text: 'That was easy!'
-// }
-
-// function sendMail () {
-//     transporter.sendMail(mailOptions, (error, info) => {
-//         if (error) {
-//             console.log(error)
-//         }
-//         else {
-//             console.log('Email sent: ' + info.response)
-//         }
-//     })
-// }
-
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
     console.log('First logs')
@@ -65,12 +37,68 @@ exports.order = functions.https.onRequest((request, response) => {
             console.error(err)
             return response.status(404).send({ error: 'unable to store', err })
         }
-        // sendMail();
     }
     else if (request.method === 'PUT') {
+
         return response.send('updatn mogsch??')
     }
 
     return response.send('GEA LERN AMOL WIA DE REQUESTS ZU MOCHN SEIN')
 })
 
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: Config.MAIL_ADDRESS,
+        pass: Config.MAIL_PASS
+    }
+})
+
+function sendMail (mailOptions) {
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error)
+        }
+        else {
+            console.log('Email sent: ' + info.response)
+        }
+    })
+}
+
+function getUpdateToken (docId) {
+    return Crypto.createHash('md5').update(`${Config.SALT}${docId}`)
+        .digest('hex')
+}
+
+// Listens for new orders that are created and send a mail to the shop.
+exports.createOrder = functions.firestore
+    .document(`${ORDERS}/{docId}`)
+    .onCreate((snap,) => {
+        // Get an object representing the document
+        const order = snap.data()
+        const updateToken = getUpdateToken(snap.id)
+
+
+        if (order.supplier_email) {
+            const supplier = Config.MAIL_ADDRESS //order.supplier_email
+            const link = `https://localhost:1234/order/${snap.id}?update_token=${updateToken}`
+            const mailOptions = {
+                from: 'bringr@gmail.com',
+                to: supplier,
+                subject: 'New Order for you',
+                text: `Link: ${link}`
+            }
+            console.log('About to send a mail')
+            try {
+                sendMail(mailOptions)
+                console.log('Mail should arrive shortly')
+            }
+            catch (err) {
+                console.log(err)
+            }
+        }
+        else {
+            console.log('no mail was provided')
+        }
+        return true
+    })
