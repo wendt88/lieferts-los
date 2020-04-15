9757e8cc5b4748e44fcdf53db72a4c0f4f4e8052
danilo <dfink91@gmail.com>
2020-04-15 21:14:22 +0200

    only import needed modules from firebase -200k

--- client/js/auth/index.js
+++ client/js/auth/index.js
@@ -1,4 +1,6 @@
-import * as firebase from 'firebase'
+import * as firebase from 'firebase/app'
+import 'firebase/auth';
+import 'firebase/firestore';
 
 const auth = {
     currentUserId () {
--- client/js/db/index.js
+++ client/js/db/index.js
@@ -1,4 +1,7 @@
-import * as firebase from 'firebase'
+import * as firebase from 'firebase/app'
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/functions';
 import auth from '../auth'
 import config from '../config'
 
