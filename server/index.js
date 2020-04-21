df65e3e1717e1f35decb4e90df1195da1b718aae
wendt88 <wendt88@gmail.com>
2020-04-21 20:04:19 +0200

    node server fixed for all paths

--- server/index.js
+++ server/index.js
@@ -1,6 +1,23 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+const fs = require('fs')
 
 app.use(express.static('output'))
 
-app.listen(process.env.PORT || 3000)
+app.get('/:language([a-z]{2})?/:path?', (req, res) => {
+    let p = getFilePath(req.params.language)
+    fs.exists(path, exists => {
+        if (!exists)
+            p = getFilePath()
+        res.sendFile(path.resolve(p))
+    })
+})
+
+function getFilePath (language) {
+    return `./output${language ? `/${language}` : ''}/index.html`
+}
+
+const port = process.env.PORT || 3000
+app.listen(port)
+console.log('port', port)
